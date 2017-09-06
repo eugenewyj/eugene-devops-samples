bf3c3a506d05b806c98418df5c42890eed5bfcb0
Eugene Wang <fengbaoxp@gmail.com>
2017-09-06 17:25:48 +0800

    添加vagrant ubuntu box通过shell安装docker

---
+++ vagrant_docker_base/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "替换国内镜像..."
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+cp /vagrant/sources.list /etc/apt/sources.list
+apt-get update
+
+echo "卸载Docker老版本..."
+apt-get remove docker docker.io
+
+echo "安装Extra Packages..."
+apt-get -f install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+echo "安装HTTPS需要的包..."
+# 参考 https://yq.aliyun.com/articles/110806
+apt-get -f install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+
+echo "安装Docker CE..."
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -f install -y docker-ce
